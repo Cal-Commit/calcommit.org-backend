97fe9c2ea9370c598a8e7bc6bd7600abf599113b
Rohan Ramakrishnan <rohanramakrish@gmail.com>
2022-10-15 18:24:24 -0700

    Validation

--- src/controllers/announcements.controllers.ts
+++ src/controllers/announcements.controllers.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from 'express'
+import { validationResult } from 'express-validator'
 
 import Announcement from '../models/announcement.model'
 
 export class AnnouncementsController {
     public static async postAnnouncement(req: Request, res: Response) {
-        console.log(req.body)
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array()[0] })
+        }
+
         const { title, description, notionRef } = req.body
 
         const nonFormatDate = new Date()
--- src/routes/announcement.routes.ts
+++ src/routes/announcement.routes.ts
@@ -1,10 +1,25 @@
 import express, { Router } from 'express'
+import { body } from 'express-validator'
 
 const router: Router = express.Router()
 
 import { AnnouncementsController } from '../controllers/announcements.controllers'
 
-router.post('/post-announcement', AnnouncementsController.postAnnouncement)
+router.post(
+    '/post-announcement',
+    [
+        body('title').notEmpty().withMessage('Title is empty').isString(),
+        body('description')
+            .notEmpty()
+            .withMessage('Description is empty')
+            .isString(),
+        body('notionRef')
+            .notEmpty()
+            .withMessage('NotionRef is empty')
+            .isString(),
+    ],
+    AnnouncementsController.postAnnouncement
+)
 
 router.get('/get-announcements', AnnouncementsController.getAnnouncements)
 
